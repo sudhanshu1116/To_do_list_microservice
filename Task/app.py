0366fe2dc3bd084a25648cebaf415d449bf1e0f7
Manasvi <manasvinarayan2@gmail.com>
2023-11-27 22:11:51 +0530

    some changes

--- Task/app.py
+++ Task/app.py
@@ -3,11 +3,16 @@ from flask_restx import Api, Resource, fields
 
 app=Flask(__name__)
 api=Api(app,doc='/swagger/')
-task_ns=api.namespace('list',path='/items/',description='List')
-item_model=api.model('item',{
+
+#Renamed namespace to tasks
+task_ns=api.namespace('task',path='/tasks/',description='Task')
+
+#renamed to task, item-->taskname, added description
+item_model=api.model('task',{
     'id':fields.Integer(readonly=True,description='id'),
-    'item': fields.String(required=True, description="The Item to be completed"),
+    'taskname': fields.String(required=True, description="The Item to be completed"),
+    'description': fields.String(required=True, description="Short description of task"),
     'done': fields.Boolean(reqired=True,description="Item Completed or not"),
-    'Start_time': fields.Boolean(reqired=True,description="Start Time"),
-    'End_time':fields.Boolean(reqired=True,description="End Time")
+    'Start_time': fields.String(reqired=True,description="Start Time"),
+    'End_time':fields.String(reqired=True,description="End Time")
 })
